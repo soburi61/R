ddebedfbbfd8e79ce4ae598a85a466e01302641b
soburi <591959ant@gmail.com>
2023-05-03 00:19:32 +0900

    update

--- ex2-3.R
+++ ex2-3.R
@@ -3,6 +3,6 @@ print(table(tenki))
 counts <- table(tenki)
 percentages <- prop.table(counts) * 100
 colors <- c("orange", "gray", "blue", "green", "yellow", "lightblue")
-pie(percentages, col = colors, main = "ある週の天気", border = "white", clockwise = TRUE, labels = paste0( c("晴れ", "曇り", "雨"),round(percentages, 1)))
+pie(percentages, col = colors, main = "ある週の天気", border = "white", clockwise = TRUE, labels = paste0( c("雨", "晴れ", "曇り"),round(percentages,1),"%"))
 par(new = TRUE)
 pie(1, radius = 0.5, col = "white", border = "white", labels = "")
