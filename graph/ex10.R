b97e826a72c945088a40788ebe26044b35143c7f
soburi59 <591959ant@gmail.com>
2023-08-03 11:56:29 +0900

    update

--- graph/ex10.R
+++ graph/ex10.R
@@ -10,11 +10,22 @@ g4_mst <- mst( g4 ) # 最小全域木を求める
 tkplot( g4_mst, edge.width=E(g4_mst)$weight, edge.color="black",
         vertex.color="white", vertex.label.color="black", layout=layout_in_circle )
 # 辺の幅を重みに対応、頂点を円状に表示
+print("以下ネットワークNについて")
 print("辺の重みの和")
 print(sum( E(g4)$weight )) # 元のネットワークの辺の重みの和
 print("辺の重みの内訳")
 print(E(g4))
 print(E(g4)$weight) # 元のネットワークの辺と重みの内訳
+
+print("-----------")
+print("以下最小全域木Tについて")
+print("辺の重みの和")
+print(sum( E(g4_mst)$weight )) # 元のネットワークの辺の重みの和
+print("辺の重みの内訳")
+print(E(g4_mst))
+print(E(g4_mst)$weight) # 元のネットワークの辺と重みの内訳
+
+
 
 
 
